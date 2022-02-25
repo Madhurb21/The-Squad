5f88b2b9809a36ca81a940b7f01a9e1c16da0c9d
Preeti8549 <99955700+Preeti8549@users.noreply.github.com>
2022-02-25 12:39:51 +0530

    added all files

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Options()
+    {
+        SceneManager.LoadScene("OptionsScreen");
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("CreditsScreen");
+    }
+}
