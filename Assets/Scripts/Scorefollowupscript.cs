fa4bb824df2a5c8721d82262d8b2428ca7faf9a5
aadyau21 <aadyau21@iitk.ac.in>
2022-02-25 23:07:24 +0530

    CHANGES

--- Assets/Scripts/Scorefollowupscript.cs
+++ Assets/Scripts/Scorefollowupscript.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class Scorefollowupscript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
+    // FOR ZOMBIE 1 add this in zombie script where bullets hit zombie
     void Start()
     {
-        
+        //ONLY USE THIS BELOW WRITTEN SENTENCE OVER THERE
+        //REST OF THE PROGRAMME NEED NOT TO BE UPLOADED
+        Scorescript.scorezombie1 = +10;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         
--- Assets/Scripts/Scorescript.cs
+++ Assets/Scripts/Scorescript.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using UnityEngine
+    using UnityEngiine.UI;
 
 public class Scorescript : MonoBehaviour
 {
+    public static int scorezombie1 = 0;
+    
+    Text score;
+   
     // Start is called before the first frame update
     void Start()
     {
-        
+        score = GetComponent<Text> (); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        score.text = "Score:" + scorezombie1;
     }
 }
