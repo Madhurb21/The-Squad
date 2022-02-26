401846f9667ceac2ab6800f875d179a6d50848e6
Madhurb21 <madhurb21@iitk.ac.in>
2022-02-26 07:08:57 +0530

    correction in script

--- Assets/Scripts/Scorescript.cs
+++ Assets/Scripts/Scorescript.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine
-    using UnityEngiine.UI;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class Scorescript : MonoBehaviour
 {
