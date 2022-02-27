84a8e26a93710d533879b7583d7b59072e054f3e
shannu711 <sreesm21@iitk.ac.in>
2022-02-27 10:38:24 +0530

    road

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,14 +4,15 @@ public class PlayerMovement : MonoBehaviour
 {
 
 public Rigidbody2D rb;
-
+float forwardforce = 200f;
 public float speed = 5f ;
 
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        rb.velocity=new Vector3(rb.velocity.x,0,0);
+
+        rb.velocity=new Vector3(rb.velocity.x,forwardforce*Time.deltaTime,0);
         float horizontal = Input.GetAxisRaw("Horizontal");
         Vector3 direction = new Vector3(horizontal,0,0);
         gameObject.transform.Translate(direction.normalized*Time.deltaTime*speed);
